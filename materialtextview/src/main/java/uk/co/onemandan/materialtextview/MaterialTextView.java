adac71c965917a9065d4a5e13b39b4db226a75d5
onemandan <danielhart.d@gmail.com>
2018-06-14 18:09:43 +0100

    0.0.2: Changeable background, added accessors

--- materialtextview/src/main/java/uk/co/onemandan/materialtextview/MaterialTextView.java
+++ materialtextview/src/main/java/uk/co/onemandan/materialtextview/MaterialTextView.java
@@ -2,6 +2,8 @@ package uk.co.onemandan.materialtextview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -11,7 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MaterialTextView extends LinearLayout implements View.OnClickListener {
-    private TextView _labelView;
+    private TextView    _labelView;
     private TextView    _contentView;
     private TextView    _helperView;
     private FrameLayout _rootView;      //Root view is a FrameLayout, as setForeground is available in
@@ -25,6 +27,7 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
     private int _labelTextColour;
     private int _contentTextColour;
     private int _helperTextColour;
+    private int _backgroundColour;
 
     private boolean _keepLabelSpacing;  //Whether or not spacing should be kept if there is no label text
     private boolean _keepHelperSpacing; //"                                   " if there is no helper text
@@ -33,6 +36,7 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
     private int _DEFAULT_LABEL_TEXT_COLOUR;     //android:textColorSecondary
     private int _DEFAULT_CONTENT_TEXT_COLOUR;   //android:textColorPrimary
     private int _DEFAULT_HELPER_TEXT_COLOUR;    //colorAccent
+    private int _DEFAULT_BACKGROUND_COLOUR;     //6% Black
 
     private Drawable _DEFAULT_CLICKABLE_FOREGROUND;
 
@@ -54,20 +58,6 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
 
         handleDefaultAttributes(context);
         handleAttributes(context, attrs);
-        initViews();
-    }
-
-    private void initViews(){
-        _labelView.setTextColor(_labelTextColour);
-        _contentView.setTextColor(_contentTextColour);
-        _helperView.setTextColor(_helperTextColour);
-
-        _labelView.setText(_labelText);
-        _contentView.setText(_contentText);
-        _helperView.setText(_helperText);
-
-        handleHelperVisibility();
-        handleLabelVisibility();
     }
 
     private void handleDefaultAttributes(Context context){
@@ -84,6 +74,7 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
         _DEFAULT_CONTENT_TEXT_COLOUR  = ta.getColor(1, 0);
         _DEFAULT_HELPER_TEXT_COLOUR   = ta.getColor(2, 0);
         _DEFAULT_CLICKABLE_FOREGROUND = ta.getDrawable(3);
+        _DEFAULT_BACKGROUND_COLOUR    = context.getResources().getColor(R.color.TintBlack06);
 
         ta.recycle();
     }
@@ -92,24 +83,29 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
         TypedArray ta = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MaterialTextView,
                 0, 0);
 
-        _labelText      = ta.getString(R.styleable.MaterialTextView_mtv_labelText) == null ? "" :
-                ta.getString(R.styleable.MaterialTextView_mtv_labelText);
-        _contentText    = ta.getString(R.styleable.MaterialTextView_mtv_contentText) == null ? "" :
-                ta.getString(R.styleable.MaterialTextView_mtv_contentText);
-        _helperText     = ta.getString(R.styleable.MaterialTextView_mtv_helperText) == null ? "" :
-                ta.getString(R.styleable.MaterialTextView_mtv_helperText);
-
-        _labelTextColour    = ta.getColor(R.styleable.MaterialTextView_mtv_labelTextColor,
-                _DEFAULT_LABEL_TEXT_COLOUR);
-        _contentTextColour  = ta.getColor(R.styleable.MaterialTextView_mtv_contentTextColor,
-                _DEFAULT_CONTENT_TEXT_COLOUR);
-        _helperTextColour   = ta.getColor(R.styleable.MaterialTextView_mtv_helperTextColor,
-                _DEFAULT_HELPER_TEXT_COLOUR);
-
+        //Misc
         _keepLabelSpacing   = ta.getBoolean(R.styleable.MaterialTextView_mtv_keepLabelSpacing,
-                true);
+                false);
         _keepHelperSpacing  = ta.getBoolean(R.styleable.MaterialTextView_mtv_keepHelperSpacing,
                 false);
+
+        //Text
+        setLabelText(ta.getString(R.styleable.MaterialTextView_mtv_labelText) == null ? "" :
+                ta.getString(R.styleable.MaterialTextView_mtv_labelText));
+        setContentText(ta.getString(R.styleable.MaterialTextView_mtv_contentText) == null ? "" :
+                ta.getString(R.styleable.MaterialTextView_mtv_contentText));
+        setHelperText(ta.getString(R.styleable.MaterialTextView_mtv_helperText) == null ? "" :
+                ta.getString(R.styleable.MaterialTextView_mtv_helperText));
+
+        //Colours
+        setLabelTextColour(ta.getColor(R.styleable.MaterialTextView_mtv_labelTextColor,
+                _DEFAULT_LABEL_TEXT_COLOUR));
+        setContentTextColour(ta.getColor(R.styleable.MaterialTextView_mtv_contentTextColor,
+                _DEFAULT_CONTENT_TEXT_COLOUR));
+        setHelperTextColour(ta.getColor(R.styleable.MaterialTextView_mtv_helperTextColor,
+                _DEFAULT_HELPER_TEXT_COLOUR));
+        setBackgroundColour(ta.getColor(R.styleable.MaterialTextView_mtv_backgroundColor,
+                _DEFAULT_BACKGROUND_COLOUR));
 
         ta.recycle();
     }
@@ -152,6 +148,33 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
         _onClickListener = l;
     }
 
+    // SETTERS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    @SuppressWarnings("unused")
+    public void setBackgroundColour(int colour){
+        _backgroundColour = colour;
+        _rootView.getBackground().setColorFilter(new PorterDuffColorFilter(_backgroundColour,
+                PorterDuff.Mode.SRC_IN));
+    }
+
+    @SuppressWarnings("unused")
+    public void setLabelTextColour(int colour){
+        _labelTextColour = colour;
+        _labelView.setTextColor(_labelTextColour);
+    }
+
+    @SuppressWarnings("unused")
+    public void setContentTextColour(int colour){
+        _contentTextColour = colour;
+        _contentView.setTextColor(_contentTextColour);
+    }
+
+    @SuppressWarnings("unused")
+    public void setHelperTextColour(int colour){
+        _helperTextColour = colour;
+        _helperView.setTextColor(_helperTextColour);
+    }
+
     @SuppressWarnings("unused")
     public void setLabelText(CharSequence text){
         _labelText = text;
@@ -173,6 +196,23 @@ public class MaterialTextView extends LinearLayout implements View.OnClickListen
 
         handleHelperVisibility();
     }
+
+    // GETTERS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @SuppressWarnings("unused")
+    public int getBackgroundColour(){
+        return _backgroundColour;
+    }
+
+    @SuppressWarnings("unused")
+    public int getLabelTextColour(){ return _labelTextColour; }
+
+    @SuppressWarnings("unused")
+    public int getContentTextColour(){ return _contentTextColour; }
+
+    @SuppressWarnings("unused")
+    public int getHelperTextColour(){ return _helperTextColour; }
 
     @SuppressWarnings("unused")
     public CharSequence getLabelText(){
